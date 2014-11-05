d0dc4c2957b1ae634af9b80b2f76e016d2b2314e
Lee.HJ <hejue2@gmail.com>
2014-11-05 16:15:33 +0800

    The big change in helloworld.c

--- helloworld.c
+++ helloworld.c
@@ -1,8 +1,17 @@
 #include <stdio.h>
+#define MAX 100
 
 int main()
 {
-	printf("lalalalala\n");
+	int number[MAX], i;
+	for(i=0; i<MAX; i++)
+	{
+		if(i == 0) number[i] = 0;
+		else if(i == 1) number[i] = 1;
+		else number[i] = number[i-1] + number[i-2];
+		printf("%d ", number[i]);
+	}
+	printf("\n");
 	printf("Hello world!\n");
 	return 0;
 }
